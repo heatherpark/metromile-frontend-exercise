6400a179fb5f553bea5e16f8aa76cab433c76c97
Heather Park <hthr.prk@gmail.com>
2018-08-29 22:11:58 -0700

    Set up EditCoverage component

---
+++ src/components/EditCoverage/EditCoverage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class EditCoverage extends Component {
+  render() {
+    return (
+      <div>EditCoverage</div>
+    );
+  }
+}
+
+export default EditCoverage;
