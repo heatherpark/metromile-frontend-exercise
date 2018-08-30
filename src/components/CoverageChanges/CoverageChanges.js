77779e08e2c1ef8f56b4e2593e8f1f8406fe8946
Heather Park <hthr.prk@gmail.com>
2018-08-30 00:43:00 -0700

    Render changes in CoverageChanges

--- src/components/CoverageChanges/CoverageChanges.js
+++ src/components/CoverageChanges/CoverageChanges.js
@@ -1,9 +1,46 @@
 import React from 'react';
 
 const CoverageChanges = props => {
+  const renderLimits = () => {
+    const limits = [];
+
+    for (let key in props.chosenLimits) {
+      if (key !== 'name' && props.chosenLimits[key] !== props.currentLimits[key]) {
+        limits.push(
+          <div>
+            {getCoverageTitle(props.options, key)} 
+            {`${getOptionName(props.options, key, props.currentLimits[key])} --> ${getOptionName(props.options, key, props.chosenLimits[key])}`}
+          </div>
+        );
+      }
+    }
+
+    return limits;
+  };
+  
+  const getCoverageTitle = (coverageOptions, optionName) => {
+    for (let coverageType of coverageOptions) {
+      if (optionName === coverageType.name) {
+        return coverageType.title;
+      }
+    }
+  };
+
+  const getOptionName = (coverageOptions, coverageName, optionValue) => {
+    for (let coverageType of coverageOptions) {
+      if (coverageName === coverageType.name) {
+        for (let option of coverageType.options) {
+          if (optionValue === option.value) {
+            return option.name;
+          }
+        }
+      }
+    }
+  };
+
   return (
     <div>
-      CoverageChanges
+      {renderLimits()}
     </div>
   );
 };
