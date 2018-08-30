3711172370ea81b27c8cf10f9c8de3e1e2d96c5b
Heather Park <hthr.prk@gmail.com>
2018-08-30 00:33:30 -0700

    Write onChooseOption method

--- src/components/EditCoverage/EditCoverage.js
+++ src/components/EditCoverage/EditCoverage.js
@@ -4,10 +4,26 @@ import CoverageOptions from '../CoverageOptions/CoverageOptions';
 import CoverageChanges from '../CoverageChanges/CoverageChanges';
 
 class EditCoverage extends Component {
+  state = {
+    chosenLimits: null
+  }
+
+  onChooseOption = (coverageName, optionValue) => {
+    this.setState(prevState => ({
+      chosenLimits: {
+        ...prevState.chosenLimits,
+        [coverageName]: optionValue
+      }
+    }));
+  }
+
   render() {
     return (
       <div>
-        <CoverageOptions />
+        <CoverageOptions 
+          limits={this.state.chosenLimits || this.props.currentLimits}
+          onChooseOption={this.onChooseOption}
+          options={this.props.coverageOptions} />
         <CoverageChanges />
       </div>
     );
